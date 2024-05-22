fa9bda2755e4161217abda643d230a286d95ae5a
Mona Lisa <teodoro.pedro@usp.br>
2024-05-21 22:49:21 -0300

    Another commit

--- sistema_emprestimo/users/admin.py
+++ sistema_emprestimo/users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+
+admin.site.register(User, UserAdmin)
